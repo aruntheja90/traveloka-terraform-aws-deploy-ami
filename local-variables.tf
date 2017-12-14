ea13094fd20f810680756d4a920564a5d7c36653
Salvian Reynaldi <salvian@traveloka.com>
2017-12-14 11:17:54 +0700

    change local variable name

--- local-variables.tf
+++ local-variables.tf
@@ -1,6 +1,6 @@
 locals {
     common-pipeline-name = "${var.service-name}-deploy-ami"
     env-pipeline-name = "${local.common-pipeline-name}-${var.environment}"
-    env-plan-pipeline-name = "${local.env-pipeline-name}-plan"
-    env-apply-pipeline-name = "${local.env-pipeline-name}-apply"
+    env-plan-pipeline-name = "${local.common-pipeline-name}-plan-${var.environment}"
+    env-apply-pipeline-name = "${local.common-pipeline-name}-apply-${var.environment}"
 }
